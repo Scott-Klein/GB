b06ba4a78ca69bf885e6c4093aa0bb2f3a5de992
Scott Klein <scotty.klei@gmail.com>
2021-01-25 19:06:23 +1100

    MBC2 Ram readbytes backed my memory mapped file.

--- GB/Emulator/Cart/CartridgeMBC2.cs
+++ GB/Emulator/Cart/CartridgeMBC2.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.MemoryMappedFiles;
 
 namespace GB.Emulator
@@ -13,6 +14,7 @@ namespace GB.Emulator
         private MemoryMappedViewAccessor ram;
         public CartridgeMBC2(string romFile) : base(romFile)
         {
+            //every mbc2 has battery backed ram, so we will create a file on the disc.
             this.ram = MemoryMappedFile.CreateFromFile(romFile.Remove(romFile.IndexOf('.')), FileMode.OpenOrCreate, null, this.Info.ExRamSize * 1024).CreateViewAccessor(0, 0, MemoryMappedFileAccess.ReadWrite);
         }
 
@@ -21,17 +23,27 @@ namespace GB.Emulator
             return addr switch
             {
                 var a when a <= 0x3fff => rom[a], //0000-3FFF contains the first 16kbyte of the rom.
-                var a when a <= 0x7fff => rom[(lowBank & 0xf) + (a & 0x3fff)]
+                var a when a <= 0x7fff => rom[(lowBank & 0xf) + (a & 0x3fff)],
+                var a when a >= 0xa000 && a <= 0xa1ff => this.ram.ReadByte(addr & 0x1ff)
+
             };
+            throw new ArgumentOutOfRangeException("addr",addr, $"Tried to read 0x{addr} from an MBC2 cartridge. This implementation of ReadByte does not handle the address inputed.");
         }
 
+
+        /// <summary>
+        /// The MBC2 writes are very simple, they either enable/disable ram,
+        /// or they decide the rombank.
+        /// </summary>
+        /// <param name="addr"></param>
+        /// <param name="value"></param>
         public override void WriteByte(ushort addr, byte value)
         {
             switch (addr)
             {
                 //Ram Enable and ROM bank Number. bit 8 == 0 means we are controlling RAM
                 //bit 8 == 1 means we are controlling the rom bank number.
-                case var n when n <= 0x3fff && (value & 0x80)  == 0x80:
+                case var n when n <= 0x3fff && (value & 0x80) == 0x80:
                     //set the rom bank number
                     this.lowBank = value & 0xf;
                     if (this.lowBank == 0)
